d998fc3736fbde83b27f887919634de5c8ad8d5b
Mabo Rustam <kimlael01@mail.ru>
2024-10-14 15:06:40 +0600

    created function handleSubmit for Post Message

--- src/containers/Blog/Blog.tsx
+++ src/containers/Blog/Blog.tsx
@@ -30,14 +30,52 @@ const Blog = () => {
 
 
 
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
 
+        try {
+            const data = new URLSearchParams();
+            data.set('message', newMessage);
+            data.set('author', newAuthor);
+
+            const response = await fetch(url, {
+                method: 'post',
+                body: data,
+            });
+
+            if (response.ok) {
+                const data = await response.json() as BlogPost;
+                setPosts((prevPosts) => [...prevPosts, data]);
+                setNewMessage('');
+            }
+        } catch (error) {
+            console.error('Ошибка при отправке сообщения:', error);
+        }
+    };
 
 
     return (
         <div className='container'>
             <div className="inner-container">
                 <div className="Posts">
+                    <form onSubmit={handleSubmit}>
+                        <div className="input-div">
+                            <input
+                                type="text"
+                                placeholder="Введите сообщение"
+                                value={newMessage}
+                                onChange={(event) => setNewMessage(event.target.value)}
+                            />
+                            <input
+                                type="text"
+                                placeholder="Введите автора"
+                                value={newAuthor}
+                                onChange={(event) => setNewAuthor(event.target.value)}
+                            />
+                        </div>
 
+                        <button type="submit">Отправить</button>
+                    </form>
                 </div>
                 <div className="Posts-Get">
                     {posts.map(post => (
